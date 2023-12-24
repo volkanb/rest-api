9805dc5d4df289eae21897489dfaf786ea2596f0
volkanb <volkanbenli92@gmail.com>
2023-12-24 01:50:03 -0500

    Add root route handler

---
+++ src/routes/root.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export function root(request: Request, response: Response) {
+
+  response.status(200).send("<h1>Express server is up and running.<h1>");
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,23 @@
-console.log(`Hello World!`);
+import * as express from 'express';
+import { root } from './routes/root';
+
+const app = express();
+
+function setupExpress () {
+
+  app.route("/").get(root);
+
+}
+
+function startServer() {
+
+  app.listen(9000, () => {
+    
+    console.log(`HTTP REST API Server is now running at http://localhost:9000`);
+
+  });
+
+}
+
+setupExpress();
+startServer();
