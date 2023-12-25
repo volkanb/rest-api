802a19bf8f6cba0a5b19a93f47bdc5ba17aa357f
volkanb <volkanbenli92@gmail.com>
2023-12-24 21:56:07 -0500

    Make port number configurable through command line

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,28 @@
 import * as express from 'express';
 import { root } from './routes/root';
+import { isInteger } from './utils';
 
 const app = express();
 
 function setupExpress () {
-
   app.route("/").get(root);
-
 }
 
 function startServer() {
+  let port: number;
+  const portArg = process.argv[2];
 
-  app.listen(9000, () => {
-    
-    console.log(`HTTP REST API Server is now running at http://localhost:9000`);
+  if(isInteger(portArg)) {
+    port = parseInt(portArg);
+  }
 
+  if(!port) {
+    port = 9000;
+  }
+
+  app.listen(port, () => {
+    console.log(`HTTP REST API Server is now running at http://localhost:${port}`);
   });
-
 }
 
 setupExpress();
