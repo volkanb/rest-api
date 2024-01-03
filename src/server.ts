5d3a0a3d46d0958c47a7b966a424a0efb05ceebf
volkanb <volkanbenli92@gmail.com>
2024-01-03 03:57:32 -0500

    Adds findCourseByUrl endpoint

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ import { logger } from './logger';
 import { AppDataSource } from './data-source';
 import { getAllCourses } from './routes/get-all-courses';
 import { defaultErrorHandler } from './middlewares/default-error-handler';
+import { findCourseByUrl } from './routes/find-course-by-url';
 
 const cors = require("cors");
 
@@ -23,6 +24,7 @@ function setupExpress () {
   app.use(cors({origin: true}));
   app.route("/").get(root);
   app.route("/api/courses").get(getAllCourses);
+  app.route("/api/courses/:courseUrl").get(findCourseByUrl);
   app.use(defaultErrorHandler);
 }
 
